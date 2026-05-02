862203bfd0e66ed30937edb714830b4c35c62577
ashishar <ashishk1@hotmail.com>
2026-05-02 20:32:49 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const repo = "Online_Encyclopedia_Mathematical_Objects_Structured";
+
+const nextConfig = {
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
+};
 
 export default nextConfig;
